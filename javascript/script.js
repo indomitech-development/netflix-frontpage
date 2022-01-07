8fcfc2e03d5761928998e8a40f092a368e289295
haseebdeveloper19 <haseebdeveloper19@gmail.com>
2022-01-08 00:18:29 +0500

    with signup page

--- javascript/script.js
+++ javascript/script.js
@@ -12,14 +12,8 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+ 
+// function textchange(){
+// document.getElementById("onclick").innerHTML="The information collected by Google reCAPTCHA is subject to the Google Privacy Policy and Terms of Service, and is used for providing, maintaining, and improving the reCAPTCHA service and for general security purposes (it is not used for personalized advertising by Google)."
 
-var docWidth = document.documentElement.offsetWidth;
-
-[].forEach.call(
-  document.querySelectorAll('*'),
-  function(el) {
-    if (el.offsetWidth > docWidth) {
-      console.log(el);
-    }
-  }
-);
+// }
